1e75162f3b84d007126a546249efec5edb157b15
EllinaSohnenko <Sohnenko.vyatsu@gmail.com>
2023-06-04 01:39:40 +0300

    Слияние ветки feature-xml в ветку dev

--- WeatherService/WeatherService/WeatherService.cpp
+++ WeatherService/WeatherService/WeatherService.cpp
@@ -1,32 +1,24 @@
 ﻿#include "Weather.h"
-<<<<<<< HEAD
 #include "JsonService.h"
-#include "nlohmann\json.hpp"
-=======
 #include "XmlService.h"
+#include "nlohmann\json.hpp"
 #include "src\pugixml.hpp"
->>>>>>> feature-xml
 #include <fstream>
 #include <string>
 #include <iostream>
 
-<<<<<<< HEAD
+using namespace pugi;
 using namespace std;
 using nlohmann::json;
 
 int main() {
     setlocale(LC_ALL, "Ru");
+    cout << "json:\n";
     JsonService js;
     Weather w = js.getWeather("weather.json");
     w.print();
-=======
-using namespace pugi;
-using namespace std;
-
-int main() {
-    setlocale(LC_ALL, "Ru");
+    cout << "\nxml:\n";
     XmlService xs;
     Weather w1 = xs.getWeather("weather.xml");
     w1.print();
->>>>>>> feature-xml
 }
